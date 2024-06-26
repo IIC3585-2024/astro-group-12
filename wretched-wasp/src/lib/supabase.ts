98ca544fa0064061dcb1f0c67ee1c5cfbfe3b806
CristobalPerez-Cotapos <cprezcotapos@uc.cl>
2024-06-26 04:02:35 -0400

    subiendo y comentarios y series con imagenes

--- wretched-wasp/src/lib/supabase.ts
+++ wretched-wasp/src/lib/supabase.ts
@@ -1,6 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
+const SUPABASE_URL = "https://qybfxlbdjylxlcjntlwa.supabase.co"
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF5YmZ4bGJkanlseGxjam50bHdhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTkyNzI1MTIsImV4cCI6MjAzNDg0ODUxMn0.rMPNwWx4GRS3wiv3ygaptNpkkgdZi1CJxbGlGYTk8IE"
+
 export const supabase = createClient(
-  import.meta.env.SUPABASE_URL,
-  import.meta.env.SUPABASE_ANON_KEY,
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
 );
--- wretched-wasp/src/stores/stores.js
+++ wretched-wasp/src/stores/stores.js
@@ -1,6 +1,6 @@
 import  { writable } from 'svelte/store';
 import { filterMovies, filterSeries } from '../utils/filters.js';
-import { createClient } from '@supabase/supabase-js';
+import { fetchAllComments, fetchSeries, fetchComments} from '../stores/supabase.js';
 
 import allMovies from '../api/dummyMovies.json';
 import allGenres from '../api/dummyGenres.json';
@@ -8,25 +8,11 @@ import allStreamingProviders from '../api/dummyStreamingProviders.json';
 import allStars from '../api/dummyStars.json';
 
 
-const SUPABASE_URL = "https://qybfxlbdjylxlcjntlwa.supabase.co"
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF5YmZ4bGJkanlseGxjam50bHdhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTkyNzI1MTIsImV4cCI6MjAzNDg0ODUxMn0.rMPNwWx4GRS3wiv3ygaptNpkkgdZi1CJxbGlGYTk8IE"
-const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 
-const fetchSeries = async () => {
-  try {
-    const { data, error } = await supabase.from('series').select('*');
-    if (error) {
-      throw error;
-    }
-    console.log('Series obtenidas:', data);
-    return data;
-  } catch (error) {
-    console.error('Error al obtener series:', error.message);
-  }
-};
-
+export const comments = writable([]);
 const series = await fetchSeries();
+
 
 export const seriesList = writable(series);
 export const moviesList = writable(allMovies);
